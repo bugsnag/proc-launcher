bdab4bbcb8b4bd233b2dc12aeb2792d220f78e5c
Delisa Mason <iskanamagus@gmail.com>
2021-01-06 12:22:59 +0000

    test: ensure fixture output flush

--- features/fixtures/extension.go
+++ features/fixtures/extension.go
@@ -1,33 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"sync"
 
 	l "github.com/bugsnag/proc-launcher/launcher"
 )
 
-type extension struct{}
+type extension struct{
+	buffer *bufio.Writer
+	group *sync.WaitGroup
+}
 
 func (ex extension) ReadStdout(bytes []byte) {
-	fmt.Printf("you said: %s", string(bytes))
+	ex.write("you said: %s\n", string(bytes))
 }
 
 func (ex extension) ReadStderr(bytes []byte) {
-	fmt.Printf("something bad happened: %s", string(bytes))
+	ex.write("something bad happened: %s\n", string(bytes))
 }
 
 func (ex extension) AtExit(code int) {
-	fmt.Printf("process terminated, code %d", code)
+	ex.write("process terminated, code %d\n", code)
+	ex.group.Done()
+}
+
+func (ex extension) write(format string, args ...interface{}) {
+	defer ex.buffer.Flush()
+
+	ex.buffer.WriteString(fmt.Sprintf(format, args...))
+}
+
+func New() extension {
+	ex := extension{
+		bufio.NewWriter(os.Stdout),
+		&sync.WaitGroup{},
+	}
+	
+	ex.group.Add(1)
+	return ex
 }
 
 func main() {
 	launcher := l.New(os.Args[1:]...)
-	launcher.InstallPlugin(extension{})
+	ex := New()
+	launcher.InstallPlugin(ex)
 	if err := launcher.Start(); err != nil {
-		fmt.Printf("failed to launch process: %v", err)
+		fmt.Printf("failed to launch process: %v\n", err)
 	}
 	if err := launcher.Wait(); err != nil {
-		fmt.Printf("failed to await process: %v", err)
+		fmt.Printf("failed to await process: %v\n", err)
 	}
+	ex.group.Wait()
 }
